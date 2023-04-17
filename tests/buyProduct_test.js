69467317ca6d1502516ba894363c234cc7956b46
adudyak <adudyak@gmail.com>
2023-04-17 15:03:37 +0300

    rm email

--- tests/buyProduct_test.js
+++ tests/buyProduct_test.js
@@ -1,13 +1,13 @@
 const USER = {
-    email: "anastasiia0.2687341244603123@mailinator.com",
-    password: "50p8c0ze",
+    email: "",
+    password: "",
 }
 
 Feature('purchase');
 
 Scenario('buy product', ({ I, basePage }) => {
-    //I.login(USER);
-    //I.amOnPage("http://opencart.qatestlab.net/index.php?route=product/product&product_id=43");
+    I.login(USER);
+    I.amOnPage("http://opencart.qatestlab.net/index.php?route=product/product&product_id=43");
     //add method to clear cart - .grabNumberOfVisibleElements();
     //select size, color. Try to select with .selectOption()
     //get product price, get size price?, get color price?, return sum of prices for single product - totalPrice
@@ -17,7 +17,7 @@ Scenario('buy product', ({ I, basePage }) => {
     //complete all steps to purchase product
     //get "Flat Shipping Rate" price - shippingRate
     //get total checkout price - checkoutPrice
-    //I.assertEqual(totalPrice + shippingRate, checkoutPrice, "Prices are not in match");
     //npm i codeceptjs-chai - install chai, https://www.npmjs.com/package/codeceptjs-chai
+    I.assertEqual(totalPrice + shippingRate, checkoutPrice, "Prices are not in match");
     //verify final text "Your order has been placed!";
 }).tag("buy");
